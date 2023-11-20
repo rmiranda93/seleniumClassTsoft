865682c220880823572d0281d2c9b1d183466490
rmiranda93 <rafael.emirandaperez@gmail.com>
2023-11-20 16:57:47 -0300

    se eliminan lineas de webdriver

--- src/test/java/codigo/WebElements.java
+++ src/test/java/codigo/WebElements.java
@@ -8,13 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebElements {
     public static void main(String[] args) throws InterruptedException{
 
-        //String rutaProyecto = System.getProperty("user.dir"); //Parametrizar el driver
-
-        //String rutaDriver = rutaProyecto+"\\src\\test\\resources\\drivers\\chromedriver.exe";
-
-        //Enlazar el driver via property en el sistema
-        //System.setProperty("webdriver.chrome.driver",rutaDriver);
-
         //Instanciar objetos para emular el browser
         WebDriver driver = new ChromeDriver();
 
